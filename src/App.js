13b53fdbcf8cbdd4a1064829d4a78df39c039480
Aleksandra Wos <aleksandra1.wos@gmail.com>
2022-04-07 13:33:39 +0100

    raw table inserted, data mapped initially, badges and logo to be added

--- src/App.js
+++ src/App.js
@@ -1,39 +1,30 @@
-import logo from './logo.svg';
+import logo from "./logo.svg";
 import Navbar from "./components/Navbar";
-import {Route, Routes} from "react-router-dom"
+import { Route, Routes } from "react-router-dom";
 import ListEvents from "./components/ListEvents";
-import {useState} from "react";
-import {useEffect} from "react";
-import axios from 'axios';
-import './App.css';
+import { useState } from "react";
+import { useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
-
   const [events, setEvents] = useState();
 
-  const url = "https://www.thesportsdb.com/api/v1/json/2/eventsseason.php?id=4328&s=2020-2021";
+  const url =
+    "https://www.thesportsdb.com/api/v1/json/2/eventsseason.php?id=4328&s=2020-2021";
 
   useEffect(() => {
     axios
       .get(url)
       .then((response) => {
         setEvents(response.data.events);
-        
-               
       })
-      .catch( err => {
-        console.log(err)
+      .catch((err) => {
+        console.log(err);
       });
-  },[]);
+  }, []);
 
-  
-
-  return (
-    <div className="App">
-     
-     {events && <ListEvents data={events}/>}
-    </div>
-  );
+  return <div className="App">{events && <ListEvents data={events} />}</div>;
 }
 
 export default App;
--- src/components/ListEvents.js
+++ src/components/ListEvents.js
@@ -1,18 +1,41 @@
-import React from 'react';
-import { useState } from 'react';
-import {useEffect} from 'react';
-import axios from 'axios';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
 export default function ListEvents(props) {
+  let eventsArr = props.data;
 
-let eventsArr = props.data
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Date</th>
+            <th>Home Team</th>
+            <th>Score</th>
+            <th>Away Team</th>
+            <th>Venue</th>
+          </tr>
+        </thead>
+        <tbody>
+          {eventsArr.map((element) => {
+            return (
+              <tr key={element.idEvent}>
+                <td>{element.dateEvent}</td>
+                <td>{element.strHomeTeam}</td>
 
-return (
-  eventsArr.map(element => {
-    return ( <p key={element.idEvent}>{element.strEvent}</p> )
-  })
-)
-   
- 
+                <td>
+                  {element.intHomeScore} : {element.intAwayScore}
+                </td>
+                <td>{element.strAwayTeam}</td>
+                <td>{element.strVenue}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
 }
